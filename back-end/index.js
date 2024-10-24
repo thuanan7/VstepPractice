dd84e249f94c0f00706c588d5966fc43aedb1a35
Khuong Nguyen <khuong.nguyen.duy@gosell.vn>
2024-10-24 08:29:19 +0700

    add exam models

--- back-end/index.js
+++ back-end/index.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.API_PORT || 4000;
-app.use('/', require('./back-end/routes/rootRouter'));
-app.use('/exams', require('./back-end/routes/examRouter'));
+app.use('/', require('./routes/rootRouter'));
+app.use('/exams', require('./routes/examRouter'));
 app.listen(port, () => {
     console.log(`server start at port: ${port}`);
 });
--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,11 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist','back-end'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['front-end/**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
