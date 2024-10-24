03472525a731629b65d29e35f91df60a3be2cf7f
Khuong Nguyen <khuong.nguyen.duy@gosell.vn>
2024-10-24 08:39:55 +0700

    add exam models

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.API_PORT || 4000;
-app.use('/', require('./routes/rootRouter'));
-app.use('/exams', require('./routes/examRouter'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.use('/api/root', require('./back-end/routes/rootRouter'));
+app.use('/api/exams', require('./back-end/routes/examRouter'));
 app.listen(port, () => {
     console.log(`server start at port: ${port}`);
 });
