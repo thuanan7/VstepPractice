4b264df291b352c27ecee2bcaadd7e8fe042b205
Khuong Nguyen <khuong.nguyen.duy@gosell.vn>
2024-10-27 08:16:53 +0700

    add authen

--- front-end/app/store/index.ts
+++ front-end/app/store/index.ts
@@ -1,22 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { rootReducer } from './reducers';
-const persistConfig = {
-    key: 'root',
-    storage,
-    whitelist: ['auth'],
-};
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+import { configureStore } from '@reduxjs/toolkit'
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistStore,
+} from 'redux-persist'
+import { rootReducer } from './reducers'
 export const store = configureStore({
-    reducer: persistedReducer,
-    middleware: getDefaultMiddleware =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }),
-});
-export const persistor = persistStore(store);
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+export const persistor = persistStore(store)
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
--- front-end/app/store/reducers.ts
+++ front-end/app/store/reducers.ts
@@ -1,9 +1,14 @@
-import { combineReducers, Reducer } from 'redux'
-import { authSlice } from '@/features/auth/authSlice.ts'
-const appReducer = combineReducers({
-  auth: authSlice.reducer,
-})
-export const rootReducer: Reducer = (state: any, action: any) => {
-  // @ts-ignore
-  return appReducer(state, action)
+import { combineReducers } from 'redux'
+import { authSlice } from '@/features/auth/authSlice'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import authTransform from './authTransform'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  transforms: [authTransform],
 }
+export const rootReducer = combineReducers({
+  auth: persistReducer(persistConfig, authSlice.reducer),
+})
