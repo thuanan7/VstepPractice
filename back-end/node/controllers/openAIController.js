419493b64719c7679fa7752ec14593b2d4bbeba6
Khuong Nguyen <khuong.nguyen.duy@gosell.vn>
2024-11-15 14:28:02 +0700

    add swagger for nodejs

--- back-end/node/controllers/openAIController.js
+++ back-end/node/controllers/openAIController.js
@@ -5,7 +5,6 @@ openAIController.testConnection = async (req, res) => {
   try {
     const host = `http://localhost:${process.env.NETCORE_PORT}/api/v1/StudentAttempt/test`
     const token = getGatewayJwtToken()
-    console.log('url', host)
     const response = await axios({
       method: req.method,
       url: host,
@@ -22,4 +21,109 @@ openAIController.testConnection = async (req, res) => {
       .json({ error: 'Error calling .NET Core API' })
   }
 }
+openAIController.startStudentAttempt = async (req, res) => {
+  try {
+    const host = `http://localhost:${process.env.NETCORE_PORT}/api/v1/StudentAttempt/start`
+    const token = getGatewayJwtToken()
+    const response = await axios({
+      method: req.method,
+      url: host,
+      data: req.body,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    res.status(response.status).json(response.data)
+  } catch (error) {
+    console.error('Error calling .NET Core API:', error.message)
+    res
+      .status(error.response ? error.response.status : 500)
+      .json({ error: 'Error calling .NET Core API' })
+  }
+}
+
+openAIController.submitStudentAnswer = async (req, res) => {
+  try {
+    let id = isNaN(req.params.attemptId)
+      ? 0
+      : parseInt(`${req.params.attemptId}`)
+    if (id > 0) {
+      const host = `http://localhost:${process.env.NETCORE_PORT}/api/v1/StudentAttempt/${id}/submit-answer`
+      const token = getGatewayJwtToken()
+      const response = await axios({
+        method: req.method,
+        url: host,
+        data: req.body,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      res.status(response.status).json(response.data)
+    } else {
+      res.status(500).json({ error: 'Please send your attemptId' })
+    }
+  } catch (error) {
+    console.error('Error calling .NET Core API:', error.message)
+    res
+      .status(error.response ? error.response.status : 500)
+      .json({ error: 'Error calling .NET Core API' })
+  }
+}
+
+openAIController.finishStudentAnswer = async (req, res) => {
+  try {
+    let id = isNaN(req.params.attemptId)
+      ? 0
+      : parseInt(`${req.params.attemptId}`)
+    if (id > 0) {
+      const host = `http://localhost:${process.env.NETCORE_PORT}/api/v1/StudentAttempt/${id}/finish`
+      const token = getGatewayJwtToken()
+      const response = await axios({
+        method: req.method,
+        url: host,
+        data: req.body,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      res.status(response.status).json(response.data)
+    } else {
+      res.status(500).json({ error: 'Please send your attemptId' })
+    }
+  } catch (error) {
+    console.error('Error calling .NET Core API:', error.message)
+    res
+      .status(error.response ? error.response.status : 500)
+      .json({ error: 'Error calling .NET Core API' })
+  }
+}
+
+openAIController.resultStudentAnswer = async (req, res) => {
+  try {
+    let id = isNaN(req.params.attemptId)
+      ? 0
+      : parseInt(`${req.params.attemptId}`)
+    if (id > 0) {
+      const host = `http://localhost:${process.env.NETCORE_PORT}/api/v1/StudentAttempt/${id}/result`
+      const token = getGatewayJwtToken()
+      const response = await axios({
+        method: req.method,
+        url: host,
+        data: req.body,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      res.status(response.status).json(response.data)
+    } else {
+      res.status(500).json({ error: 'Please send your attemptId' })
+    }
+  } catch (error) {
+    console.error('Error calling .NET Core API:', error.message)
+    res
+      .status(error.response ? error.response.status : 500)
+      .json({ error: 'Error calling .NET Core API' })
+  }
+}
+
 module.exports = openAIController
--- back-end/node/routes/openAIRouter.js
+++ back-end/node/routes/openAIRouter.js
@@ -25,4 +25,125 @@ const authMiddleware = require('../middlewares/authMiddleware')
  *         description: Unauthorized
  */
 router.get('/test', authMiddleware, openAIController.testConnection)
+
+/**
+ * @swagger
+ * /ai/start:
+ *   post:
+ *     summary: Start Student Attempt
+ *     tags: [AI]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description:need add code
+ *       401:
+ *         description: Unauthorized
+ */
+router.post('/start', authMiddleware, openAIController.startStudentAttempt)
+
+/**
+ * @swagger
+ * /ai/{attemptId}/submit-answer:
+ *   post:
+ *     summary: Start Student Attempt
+ *     description: Submit an answer for a specific student attempt
+ *     parameters:
+ *       - in: path
+ *         name: attemptId
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: ID of the attempt
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               QuestionId:
+ *                 type: integer
+ *                 description: ID of the question
+ *                 example: 1
+ *               SelectedOptionId:
+ *                 type: integer
+ *                 nullable: true
+ *                 description: ID of the selected option (for multiple choice questions)
+ *                 example: 2
+ *               EssayAnswer:
+ *                 type: string
+ *                 nullable: true
+ *                 description: Answer text for essay questions
+ *                 example: "This is my essay answer."
+ *     tags: [AI]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Answer submitted successfully
+ *       400:
+ *         description: Invalid input data
+ *       404:
+ *         description: Attempt not found
+ */
+router.post(
+  '/:attemptId/submit-answer',
+  authMiddleware,
+  openAIController.submitStudentAnswer,
+)
+
+/**
+ * @swagger
+ * /ai/{attemptId}/finish:
+ *   post:
+ *     summary: finish Student Attempt
+ *     parameters:
+ *       - in: path
+ *         name: attemptId
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: ID of the attempt
+ *     tags: [AI]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description:need add code
+ *       401:
+ *         description: Unauthorized
+ */
+router.post(
+  '/:attemptId/finish',
+  authMiddleware,
+  openAIController.finishStudentAnswer,
+)
+
+/**
+ * @swagger
+ * /ai/{attemptId}/result:
+ *   post:
+ *     summary: result Student Attempt
+ *     parameters:
+ *       - in: path
+ *         name: attemptId
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: ID of the attempt
+ *     tags: [AI]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description:need add code
+ *       401:
+ *         description: Unauthorized
+ */
+router.post(
+  '/:attemptId/result',
+  authMiddleware,
+  openAIController.resultStudentAnswer,
+)
 module.exports = router
