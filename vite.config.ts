6389fe3716cde0361f96c2fb059c49e108b20b17
Khuong Nguyen <khuong.nguyen.duy@gosell.vn>
2024-12-17 11:52:36 +0700

    add code static.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+import path from 'path'
 // https://vitejs.dev/config/
 
-const port = process.env.WEB_PORT || 4002;
+const port = process.env.WEB_PORT || 4002
 export default defineConfig({
   server: {
-    port:parseInt(`${port}`),
+    port: parseInt(`${port}`),
+    middlewareMode: true,
+    hmr: true,
+    fs: {
+      strict: false,
+    },
   },
   resolve: {
     alias: { '@': path.resolve(__dirname, 'front-end/') },
